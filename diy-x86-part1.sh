e3a1b8df796af0c13940f45ebe412946a02a2176
wekingchen <weking.chen@gmail.com>
2022-02-28 17:21:14 +0800

    Create diy-x86-part1.sh

---
+++ diy-x86-part1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 替换默认IP
+sed -i 's#192.168.1.1#10.10.10.252#g' package/base-files/files/bin/config_generate
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
