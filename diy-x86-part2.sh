026942a22bbdf6379ae0da86422bb074d189383e
wekingchen <weking.chen@gmail.com>
2022-04-13 11:17:39 +0800

    Update diy-x86-part2.sh

--- diy-x86-part2.sh
+++ diy-x86-part2.sh
@@ -27,5 +27,9 @@ sed -i 'set network.$1.gateway='10.10.10.251'' package/base-files/files/bin/conf
 git clone https://github.com/jerrykuku/go-aliyundrive-webdav package/go-aliyundrive-webdav
 git clone https://github.com/jerrykuku/luci-app-go-aliyundrive-webdav package/luci-app-go-aliyundrive-webdav
 
+# 添加aliyundrive-webdav
+svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav feeds/packages/net/aliyundrive-webdav
+svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications/luci-app-aliyundrive-webdav
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
