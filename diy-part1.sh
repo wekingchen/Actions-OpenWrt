62cbdde3be8c84da61f432f8a36470c85f927fd1
wekingchen <weking.chen@gmail.com>
2023-09-18 18:36:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,8 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 # 解决helloworld源码缺失
 # wget 'https://github.com/wekingchen/Actions-OpenWrt/raw/main/myfiles/ssr.zip' --no-check-certificate && sudo unzip -o ssr.zip && sudo rm -f ssr.zip
