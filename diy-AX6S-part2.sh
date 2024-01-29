795348b9b74cae46a99cbee84837511a5a9ac8b5
wekingchen <weking.chen@gmail.com>
2024-01-29 14:21:52 +0800

    Update diy-AX6S-part2.sh

--- diy-AX6S-part2.sh
+++ diy-AX6S-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# 添加aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+git clone https://github.com/messense/aliyundrive-webdav.git
+cp -r aliyundrive-webdav/openwrt/aliyundrive-webdav feeds/packages/multimedia
+cp -r aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications
+rm -rf aliyundrive-webdav
