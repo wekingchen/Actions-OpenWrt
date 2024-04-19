b27df8a66c7973f4a17f8727e1629758182e8c5a
wekingchen <weking.chen@gmail.com>
2024-04-20 00:28:41 +0800

    Update diy-x86-part2.sh

--- diy-x86-part2.sh
+++ diy-x86-part2.sh
@@ -67,7 +67,7 @@ git clone https://github.com/VictC79/luci-app-vssr.git package/luci-app-vssr
 
 # 添加Cloudflared Zero Trust Tunnel
 git clone https://github.com/openwrt/luci.git openwrt-luci
-cp -r openwrt-luci/applications/luci-app-cloudflared package
+cp -r openwrt-luci/applications/luci-app-cloudflared feeds/luci/applications
 rm -rf openwrt-luci
 git clone https://github.com/openwrt/packages.git openwrt-packages
 rm -rf feeds/packages/net/cloudflared
