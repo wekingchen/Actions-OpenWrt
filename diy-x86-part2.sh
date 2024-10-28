f7c9c359d64e766669c0cc19ce2b76d779068830
wekingchen <weking.chen@gmail.com>
2024-10-28 13:07:36 +0800

    Update diy-x86-part2.sh

--- diy-x86-part2.sh
+++ diy-x86-part2.sh
@@ -23,8 +23,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
 
 # 修改frp版本为官网最新v0.61.0 https://github.com/fatedier/frp 格式：https://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
-sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.61.0/' feeds/packages/net/frp/Makefile
-sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=c06a11982ef548372038ec99a6b01cf4f7817a9b88ee5064e41e5132d0ccb7e1/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_VERSION:=0.51.3/PKG_VERSION:=0.61.0/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_HASH:=83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef/PKG_HASH:=c06a11982ef548372038ec99a6b01cf4f7817a9b88ee5064e41e5132d0ccb7e1/' feeds/packages/net/frp/Makefile
 
 # 修改tailscale版本为官网最新v1.76.0 https://github.com/tailscale/tailscale 格式：https://codeload.github.com/tailscale/tailscale/tar.gz/v$(PKG_VERSION)?
 sed -i 's/PKG_VERSION:=1.70.0/PKG_VERSION:=1.76.0/' feeds/packages/net/tailscale/Makefile
