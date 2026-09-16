2bdb9c1e82d55cfd2e436c97afbd1bd95dd87b2f
wekingchen <weking.chen@gmail.com>
2026-09-16 21:32:14 +0800

    Update diy-x86-part1.sh

--- diy-x86-part1.sh
+++ diy-x86-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
